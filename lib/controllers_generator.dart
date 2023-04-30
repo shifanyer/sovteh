2546e1568cc490182b0cb98e6860cf69b248d212
shifanyer <ivashka_shir@mail.ru>
2023-04-30 16:17:05 +0300

    refactor

--- lib/controllers_generator.dart
+++ lib/controllers_generator.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:sovteh/slider_with_wrap.dart';
+import 'package:sovteh/toggle_with_wrap.dart';
+import 'package:sovteh/utils.dart';
+
+import 'message_sender.dart';
 
 class ControllersGen extends StatelessWidget {
-  const ControllersGen({super.key});
+  final List<ControllerUnit> elements;
+  final MessageSender messageSender;
+
+  const ControllersGen(
+      {super.key, required this.elements, required this.messageSender});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return ListView.builder(
+      itemCount: elements.length + 1,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return Container(
+            height: 10,
+          );
+        } else {
+          ControllerUnit controllerUnit = elements[index - 1];
+          if (controllerUnit.controllerType == ControllerType.toggle) {
+            return ToggleWithWrap(
+              messageSender: messageSender,
+              tag: controllerUnit.tag,
+              initialValue: controllerUnit.initialValue,
+            );
+          } else {
+            return SliderWithWrap(
+              messageSender: messageSender,
+              tag: controllerUnit.tag,
+              initialValue: controllerUnit.initialValue,
+            );
+          }
+        }
+      },
+    );
   }
-
 }
--- lib/controllers_page.dart
+++ lib/controllers_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sovteh/slider_with_wrap.dart';
-import 'package:sovteh/toggle_with_wrap.dart';
 import 'package:sovteh/utils.dart';
 
+import 'controllers_generator.dart';
 import 'message_sender.dart';
 
 class ControllersPage extends StatefulWidget {
@@ -26,32 +25,11 @@ class _ControllersPageState extends State<ControllersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.blue),
-        body: ListView.builder(
-          itemCount: widget.controllersList.elements.length + 1,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return Container(
-                height: 10,
-              );
-            } else {
-              ControllerUnit controllerUnit =
-                  widget.controllersList.elements[index - 1];
-              if (controllerUnit.controllerType == ControllerType.toggle) {
-                return ToggleWithWrap(
-                  messageSender: widget.messageSender,
-                  tag: controllerUnit.tag,
-                  initialValue: controllerUnit.initialValue,
-                );
-              } else {
-                return SliderWithWrap(
-                  messageSender: widget.messageSender,
-                  tag: controllerUnit.tag,
-                  initialValue: controllerUnit.initialValue,
-                );
-              }
-            }
-          },
-        ));
+      appBar: AppBar(backgroundColor: Colors.blue),
+      body: ControllersGen(
+        elements: widget.controllersList.elements,
+        messageSender: widget.messageSender,
+      ),
+    );
   }
 }
